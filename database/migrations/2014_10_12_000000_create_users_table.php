060c921b59ba72950b6049bfb71d079be29cafc8
Nasrul Hazim <nasrulhazim.m@gmail.com>
2019-03-02 11:14:48 +0800

    Added reference on E-mail Verification Setup

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,7 @@ class CreateUsersTable extends Migration
             $table->bigIncrements('id');
             $table->string('name');
             $table->string('email')->unique();
+            // Refer https://laravel.com/docs/5.8/verification
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
